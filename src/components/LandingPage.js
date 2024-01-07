32af382bd2d2419fea186e20a784f345f90aa7e5
kariem atef <katef4421@gmail.com>
2024-01-07 23:59:49 +0200

    das

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,75 +1,96 @@
-
-
 import whatsap from "../images/whatsap.png";
 import github from "../images/github.png";
 import gmail from "../images/gmail.png";
 import linkedin from "../images/linkedin.png";
 import facebook from "../images/facebook.png";
 // import landingPhoto from "../images/land.svg"
-import landingPhoto from "../images/Nerd-pana.svg"
-
+import landingPhoto from "../images/Nerd-pana.svg";
+import React from "react";
 
 // import "./PDF/CV19-8-2023.pdf";
-import { FaUserNinja  } from "react-icons/fa";
+// import { FaUserNinja } from "react-icons/fa";
 
+const LandingPage = ({ textColor }) => {
+  // const handleDownload = () => {
+  //   // const pdfUrl = 'http://localhost:3003/src/components/CV.pdf';
+  //   const pdfUrl = "https://flowcv.com/resume/sr797v0gbc";
+  //   const link = document.createElement("a");
+  //   link.href = pdfUrl;
+  //   link.target = "_blank";
+  //   // link.download = 'CV.pdf';
+  //   link.click();
+  // };
 
-const LandingPage = ({textColor})=>{
-
-    const handleDownload = () => {
-        const pdfUrl = './PDF/CV19-8-2023.pdf';
-        const link = document.createElement('a');
-        link.href = pdfUrl;
-        link.download = 'CV19-8-2023.pdf';
-        link.click();
-      };
-      
-    return(
-        <div>
-            <div className="containerLefRight">
-            <div className="leftLanding">
-                <p style={{background:textColor}} className="pForHello">Hello, i'm</p>
-                <h1 className="h1ForName">Eng.Kariem</h1>
-                <p className="pForJop">Frontend Developer</p>
-                <div className="icons">
-                    <div className="divForIcon">
-                        <img className="img1" alt="github" src={github}/>
-                    </div>
-                    <div className="divForIcon">
-                        <img className="img1" alt="facebook"src={facebook}/>
-                    </div>
-                    <div className="divForIcon">
-                        <img className="img1" alt="linkedin"src={linkedin}/>
-                    </div > 
-                    <div className="divForIcon">
-                        <img className="img1" alt="gmail"src={gmail}/>
-                    </div>
-                    <div className="divForIcon">
-                        <img className="img1" alt="whatsap"src={whatsap}/>
-                    </div>
-
-
-                    </div>
-                    <div>
-                        {/* <a href="CV19-8-2023.pdf" download="CV19-8-2023.pdf" target="_blank"> */}
-                            <button style={{background: textColor}} className="btn" onClick={handleDownload}>Download CV</button>
-                        {/* </a> */}
-                    </div>
-
+  return (
+    <div>
+      <div className="containerLefRight">
+        <div className="leftLanding">
+          <p style={{ background: textColor }} className="pForHello">
+            Hello, i'm
+          </p>
+          <h1 className="h1ForName">Eng.Kariem</h1>
+          <p className="pForJop">Frontend Developer</p>
+          <div className="icons">
+            <div className="divForIcon">
+              <a
+                target="_blank"
+                href="https://github.com/Kariem21"
+                rel="noreferrer"
+              >
+                <img className="img1" alt="github" src={github} />
+              </a>
             </div>
-            <div className="rightLanding">
-                <img style={{background:textColor}} alt="pho" src={landingPhoto}/>
-                {/* <FaUserNinja style={{background:textColor}} /> */}
+            <div className="divForIcon">
+              <a
+                target="_blank"
+                href="https://www.facebook.com/profile.php?id=100004190234760&sk=about"
+                rel="noreferrer"
+              >
+                <img className="img1" alt="facebook" src={facebook} />
+              </a>
+            </div>
+            <div className="divForIcon">
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/kariem-atef-b819171b9"
+                rel="noreferrer"
+              >
+                <img className="img1" alt="linkedin" src={linkedin} />
+              </a>
+            </div>
+            <div className="divForIcon">
+              <a href="mailto:Kariem.Atef21@gmail.com">
+                <img className="img1" alt="gmail" src={gmail} />
+              </a>
+            </div>
+            <div className="divForIcon">
+              <a target="_blank" href="01026249568" rel="noreferrer">
+                <img className="img1" alt="whatsap" src={whatsap} />
+              </a>
+            </div>
+          </div>
+          <a  className="btn"  href="https://flowcv.com/resume/sr797v0gbc" rel="noreferrer" target="_blank">MY CV</a>
 
-            </div>
-            </div>
-            {/* <div  className="divbtn">
+          {/* <div>
+            <button
+              style={{ background: textColor }}
+             
+              onClick={handleDownload}
+            >
+              MY CV
+            </button>
+          </div> */}
+        </div>
+        <div className="rightLanding">
+          <img style={{ background: textColor }} alt="pho" src={landingPhoto} />
+          {/* <FaUserNinja style={{background:textColor}} /> */}
+        </div>
+      </div>
+      {/* <div  className="divbtn">
                 <button>.</button>
             </div> */}
-            
-             
-
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default LandingPage;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,4 @@
-
-import project1 from "../images/project12.png"
+import project1 from "../images/project12.png";
 import project2 from "../images/project2.png";
 import project3 from "../images/project3.png";
 import project4 from "../images/project41.png";
@@ -8,86 +7,152 @@ import project6 from "../images/project61.png";
 import project7 from "../images/project7.png";
 import project8 from "../images/project8.png";
 import project9 from "../images/project9.png";
+import project10 from "../images/project10.png";
+import project11 from "../images/project11.png";
 
-const Portfolio = ({textColor}) => {
-    return (
-        <>
-<div style={{justifyContent:"center"}} className="contRightAbout">
-                <span  style={{background:textColor}} className="shape"></span>
-                <span style={{marginLeft:"-2%"}} className="AboutTitle"> Projects</span>
-            </div>
-        <div className="Portfolio">
-            {/* <div className="divProject"> */}
-            <div className="image-container">
-                <img src={project1} className="hover-image" alt="Hover " />
-                <div className="hover-text">
-                    Go to see This Profile Template
-                </div>
-            </div>
-            {/* </div> */}
-            {/* <div className="divProject"> */}
+const Portfolio = ({ textColor }) => {
+  return (
+    <>
+      <div style={{ justifyContent: "center" }} className="contRightAbout">
+        <span style={{ background: textColor }} className="shape"></span>
+        <span style={{ marginLeft: "-2%" }} className="AboutTitle">
+          {" "}
+          Projects
+        </span>
+      </div>
+      <div className="Portfolio">
+        {/* <div className="divProject"> */}
+        <div className="image-container">
+          <img src={project7} className="hover-image" alt="Hover " />
 
-            <div className="image-container">
-                <img src={project2} className="hover-image" alt="Hover " />
-                <div className="hover-text">
-                Go to see a similar site Noon 
-                </div>
-            </div>            {/* </div>  */}
-            {/* <div className="divProject"> */}
-            <div className="image-container">
-                <img src={project3} className="hover-image" alt="Hover " />
-                <div className="hover-text">
-                    Go to see test on js, html and css
-                </div>
-            </div>            {/* </div> <div> */}
-            <div className="image-container">
-                <img src={project4} className="hover-image" alt="Hover " />
-                <div className="hover-text">
-                    Go to see a simple ecommerce website
-                </div>
-            </div>            {/* </div> */}
-            {/* <div className="divProject"> */}
-            <div className="image-container">
+          <a
+            href="https://drive.google.com/file/d/1wyfMZSENx3zih81JOOML_y2OYImXTRcZ/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+            className="hover-text"
+          >
+            {" "}
+            My Graduation Project
+          </a>
+        </div>
+        <div className="image-container">
+          <img src={project6} className="hover-image" alt="Hover " />
+
+          <a
+            href="https://resweb.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover-text"
+          >
+            {" "}
+            ResWeb Website
+          </a>
+        </div>
+        <div className="image-container">
+          <img src={project11} className="hover-image" alt="Hover " />
+
+          <a
+            href="https://brightway-frontend.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover-text"
+          >
+            {" "}
+            BrightWay
+          </a>
+        </div>
+        <div className="image-container">
+          <img src={project1} className="hover-image" alt="Hover " />
+
+          <a
+            href="https://kariem21.github.io/Main-Project/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover-text"
+          >
+            Profile Template
+          </a>
+        </div>
+        {/* </div> */}
+        {/* <div className="divProject"> */}
+        <div className="image-container">
+          <img src={project2} className="hover-image" alt="Hover " />
+
+          <a
+            href="https://kariem-ecommerce-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover-text"
+          >
+            similar To Noon
+          </a>
+        </div>{" "}
+        {/* </div>  */}
+        {/* <div className="divProject"> */}
+        {/* <div className="image-container">
+          <img src={project3} className="hover-image" alt="Hover " />
+          <div className="hover-text">Go to see test on js, html and css</div>
+        </div>{" "} */}
+        {/* </div> <div> */}
+        <div className="image-container">
+          <img src={project4} className="hover-image" alt="Hover " />
+
+          <a
+            href="https://simple-ecommerce-kariem.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover-text"
+          >
+            Products Details
+          </a>
+        </div>{" "}
+        {/* </div> */}
+        {/* <div className="divProject"> */}
+        {/* <div className="image-container">
                 <img src={project5} className="hover-image" alt="Hover " />
                 <div className="hover-text">
                     Go to see cars website
                 </div>
-            </div>            {/* </div> */}
-            {/* <div className="divProject"> */}
-            <div className="image-container">
-                <img src={project6} className="hover-image" alt="Hover " />
-                <div className="hover-text">
-                    Go to see ResWeb Website it is For a Client 
-                </div>
-            </div>            {/* </div>  */}
-            {/* <div className="divProject"> */}
-            <div className="image-container">
-                <img src={project7} className="hover-image" alt="Hover " />
-                <div className="hover-text">
-                    Go To See My Graduation Project
-                </div>
-            </div>            {/* </div>  */}
-            {/* <div className="divProject"> */}
-            <div className="image-container">
-                <img src={project8} className="hover-image" alt="Hover " />
-                <div className="hover-text">
-                    Go to see a static dashboard
-                </div>
-            </div>            {/* </div>  */}
-            {/* <div className="divProject"> */}
-            <div className="image-container">
-                <img src={project9} className="hover-image" alt="Hover " />
-                <div className="hover-text">
-                    Go to see crud app
-                </div>
-            </div>            {/* </div> */}
+            </div>             */}
+        {/* <div className="divProject"> */}
+        {/* </div>  */}
+        {/* <div className="divProject"> */}
+        {/* </div>  */}
+        {/* <div className="divProject"> */}
+        {/* <div className="image-container">
+          <img src={project8} className="hover-image" alt="Hover " />
+          <div className="hover-text">Go to see a static dashboard</div>
+        </div>{" "} */}
+        {/* </div>  */}
+        {/* <div className="divProject"> */}
+        <div className="image-container">
+          <img src={project9} className="hover-image" alt="Hover " />
 
-
-
+          <a
+            href="https://github.com/Kariem21/CRUD-App"
+            target="_blank"
+            rel="noreferrer"
+            className="hover-text"
+          >
+            crud app
+          </a>
         </div>
-        </>
+        <div className="image-container">
+          <img src={project10} className="hover-image" alt="Hover " />
 
-    )
-}
+          <a
+            href="https://convert-currency-blue.vercel.app/"
+            className="hover-text"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Curruncy Converter
+          </a>
+        </div>
+        {/* </div> */}
+      </div>
+    </>
+  );
+};
 
 export default Portfolio;
